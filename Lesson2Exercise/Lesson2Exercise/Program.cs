af050dd8c725d7a1be8475dc7ae616fb7d4739de
NaurisB <nauris.blumbahs@gmail.com>
2021-09-07 22:04:43 +0300

    add

--- Lesson2Exercise/Lesson2Exercise/Program.cs
+++ Lesson2Exercise/Lesson2Exercise/Program.cs
@@ -7,17 +7,21 @@ namespace Lesson2Exercise
         static void Main(string[] args)
         {
             Console.WriteLine("Kā tevi sauc?");
+
             string userName = Console.ReadLine();
+
             Console.WriteLine("Sveiks, " + userName);
 
             Console.WriteLine(".....................................................................................................");
 
             Console.WriteLine("Kāds ir tavs vecums?");
+
             string userAgeText = Console.ReadLine();
             int userAge = int.Parse(userAgeText);
-            int fullAge = ++userAge;
-            string interpolatedText = $"Nākamgad tev paliks {fullAge}, Tu esi pilngadīgs ";
-            Console.WriteLine(interpolatedText);
+            int ageNextYear = ++userAge;
+            bool isAdult = userAge >= 18;
+      
+            Console.WriteLine($"Nākamgad tev paliks {ageNextYear}, Tu esi pilngadīgs: {isAdult} ");
 
             Console.WriteLine(".....................................................................................................");
 
